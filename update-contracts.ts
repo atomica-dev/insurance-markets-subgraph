ffc78124a5f593027d28ade490c4645805848ca2
Kanstantsin Svirski <kanstantsin.svirski@altoros.com>
2022-10-20 12:36:13 +0200

    - added mumbai support;

--- update-contracts.ts
+++ update-contracts.ts
@@ -20,6 +20,7 @@ const NETWORK_NAMES: any = {
   "42": "kovan",
   "1": "mainnet",
   "137": "matic",
+  "80001": "mumbai",
 };
 const MANUAL_FILE_UPDATES: string[] = [];
 const ENV = process.argv[2] || "srm-dev";
