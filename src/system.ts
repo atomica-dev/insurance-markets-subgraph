373bffb38a888c8e82dbf0c33b86c6816e7814bb
Uladzislau Miranchuk <u.miranchuk@altoros.com>
2022-12-21 12:02:33 +0300

    - add maxIterations field to System entity

--- src/system.ts
+++ src/system.ts
@@ -42,6 +42,7 @@ export function getSystemConfig(id: string): System {
     config.idleDuration = contract.idleDuration();
     config.extPoolDetailsConfidenceInterval =
       contract.externalRiskPoolsConfidenceInterval();
+    config.maxIterations = contract.maxIterations();
 
     config.save();
   }
